7ab64450eb9fea4fa2e2e14601f045cefdcfd96b
ItsOnlyCole <ItsOnlyCole@gmail.com>
2023-05-03 18:22:20 -0400

    Update Neovim Config

--- neovim/.config/nvim/lua/itsonlycole/packer.lua
+++ neovim/.config/nvim/lua/itsonlycole/packer.lua
@@ -26,6 +26,37 @@ return require('packer').startup(function(use)
 	  end
   })
 
--- Treesitter
+-- Treesitter (Syntax Highlighting)
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+-- Harpoon (File quick switch)
+  use('theprimeagen/harpoon')
+
+-- Undo Tree
+  use('mbbill/undotree')
+
+-- Vim Fugitive (Git management)
+  use('tpope/vim-fugitive')
+
+-- LSP-Zero (Autocompletion and LSP)
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  }
 end)
